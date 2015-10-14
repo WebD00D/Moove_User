80e7961269eadd7958f16f35963d433ad310702e
Christian <Bryant>
2015-10-14 15:43:16 -0400

    added location page

--- js/moove.js
+++ js/moove.js
@@ -2,6 +2,14 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+     $('select').material_select();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+
+$("#btnSignIn").click(function(){
+  window.location.href = "Location.html"
+})
