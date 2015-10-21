9ecbe21e434e08ce5fe95e736d3edb9066fa90bb
Christian <Bryant>
2015-10-21 19:58:39 -0400

    messing around with uber api

--- js/moove.js
+++ js/moove.js
@@ -1,11 +1,57 @@
+var uberClientId = "KBm1CGJRwFYKVRefpb6TwmQ5f8jamppM";
+var uberServerToken = "isuO0uEgbauTgyUDh8-DxGTLmLBWoaEIAePdyIaE";
+
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
      $('select').material_select();
 
+     alert(uberClientId);
+
+     // create placeholder variables
+     var userLatitude
+       , userLongitude;
+
+     navigator.geolocation.watchPosition(function(position) {
+         // Update latitude and longitude
+         userLatitude = position.coords.latitude;
+         userLongitude = position.coords.longitude;
+         // Query Uber API if needed
+         getEstimatesForUserLocation(userLatitude, userLongitude);
+     });
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var partyLatitude = 37.5553965;
+var partyLongitude = -77.4870686;
+
+
+
+function getEstimatesForUserLocation(latitude,longitude) {
+  $.ajax({
+    url: "https://api.uber.com/v1/estimates/price",
+    headers: {
+        Authorization: "Token " + uberServerToken
+    },
+    data: {
+        start_latitude: latitude,
+        start_longitude: longitude,
+        end_latitude: partyLatitude,
+        end_longitude: partyLongitude
+    },
+    success: function(result) {
+        console.log(result);
+    }
+  });
+}
+
+
+
+
+
 
 
 
