734ee872cd836cb0a00e7df64751b4b13ceafc14
Christian <Bryant>
2015-10-21 20:39:31 -0400

    playing with Uber methods

--- js/moove.js
+++ js/moove.js
@@ -12,14 +12,29 @@ var uberServerToken = "isuO0uEgbauTgyUDh8-DxGTLmLBWoaEIAePdyIaE";
      // create placeholder variables
      var userLatitude
        , userLongitude;
+       , timer;
+
+
+
+
 
      navigator.geolocation.watchPosition(function(position) {
          // Update latitude and longitude
          userLatitude = position.coords.latitude;
          userLongitude = position.coords.longitude;
          // Query Uber API if needed
-        // alert("about to get estimates");
-         getEstimatesForUserLocation(userLatitude, userLongitude);
+
+        // Create timer if needed
+        // Once initialized, it will fire every 60 seconds as recommended by the Uber API
+        // We only create the timer after we've gotten the user's location for the first time
+        if (typeof timer === typeof undefined) {
+          timer = setInterval(function() {
+            getEstimatesForUserLocation(userLatitude, userLongitude);
+          }, 60000);
+
+          // Query Uber API if needed
+          getEstimatesForUserLocation(userLatitude, userLongitude);
+
      });
 
 
@@ -46,6 +61,20 @@ function getEstimatesForUserLocation(latitude,longitude) {
     },
     success: function(result) {
       console.log(result);
+      var data = result["prices"];
+      if (typeof data != typeof undefined) {
+        // Sort Uber products by time to the user's location
+        data.sort(function(t0, t1) {
+        return t0.duration - t1.duration;
+        });
+
+        // Update the Uber button with the shortest time
+        var shortest = data[0];
+
+        if (typeof shortest != typeof undefined) {
+          console.log("Updating time estimate...");
+          console.log("IN " + Math.ceil(shortest.duration / 60.0) + " MIN");
+        }
     }
   });
 }
